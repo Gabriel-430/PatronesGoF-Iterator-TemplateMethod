563160c60286b8ef53ce8a89bef36a8ca9eb5edc
emilianomb07 <moralesbaiza1@gmail.com>
2026-05-03 16:24:31 -0600

    Se suben los archivos de codigo de Iterator

---
+++ src/iterator/__init__.py
@@ -0,0 +1,52 @@
+# =========================
+# ENTIDAD: Proyecto
+# =========================
+class Proyecto:
+    def __init__(self, nombre, cupo):
+        self.nombre = nombre
+        self.cupo = cupo
+
+    def __str__(self):
+        return f"{self.nombre} (Cupo: {self.cupo})"
+
+
+class ProyectoIterator:
+    def __init__(self, proyectos):
+        self._proyectos = proyectos
+        self._posicion = 0
+
+    def has_next(self):
+        return self._posicion < len(self._proyectos)
+
+    def next(self):
+        if self.has_next():
+            proyecto = self._proyectos[self._posicion]
+            self._posicion += 1
+            return proyecto
+        else:
+            raise StopIteration("No hay más proyectos")
+
+
+class ListaProyectos:
+    def __init__(self):
+        self._proyectos = []
+
+    def agregar_proyecto(self, proyecto):
+        self._proyectos.append(proyecto)
+
+    def crear_iterador(self):
+        return ProyectoIterator(self._proyectos)
+
+
+class Coordinador:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    def mostrar_proyectos(self, lista_proyectos):
+        print(f"\nProyectos disponibles para asignación:\n")
+
+        iterador = lista_proyectos.crear_iterador()
+
+        while iterador.has_next():
+            proyecto = iterador.next()
+            print(proyecto)
---
+++ src/iterator/main.py
@@ -0,0 +1,16 @@
+from __init__ import Proyecto, ListaProyectos, Coordinador
+
+def main():
+    # Crear colección de proyectos
+    lista = ListaProyectos()
+    lista.agregar_proyecto(Proyecto("App Chedrahui", 2))
+    lista.agregar_proyecto(Proyecto("Proyecto Meta", 0))
+    lista.agregar_proyecto(Proyecto("Proyecto Banco", 3))
+
+    coordinador = Coordinador("Dr. Angel")
+
+    coordinador.mostrar_proyectos(lista)
+
+
+if __name__ == "__main__":
+    main()
