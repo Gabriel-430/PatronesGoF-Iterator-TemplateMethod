e8e2238303d4d31e4e664db5d70f512c35164fb6
Gabriel <g2585esteam@gmail.com>
2026-05-07 00:16:44 -0600

    Código de Template Method mejorado para ser más flexible y estar más completo

--- src/template/__init__.py
+++ src/template/__init__.py
@@ -1,32 +1,61 @@
-class ProcesadorDocumentos:
+from abc import ABC, abstractmethod
+
+# AbstractClass
+class ProcesadorDocumentos(ABC):
+    
+    # Template Method
     def procesar_archivo(self, ruta_archivo):
         self.abrir_archivo(ruta_archivo)
         datos_sin_procesar = self.extraer_datos(ruta_archivo)
-        resultado = self.analizar_datos(datos_sin_procesar)
-        self.cerrar_archivo(ruta_archivo)
         
+        if self.requiere_analisis():
+            resultado = self.analizar_datos(datos_sin_procesar)
+        else:
+            resultado = f"Contenido extraido: {datos_sin_procesar}"
+            
+        self.cerrar_archivo(ruta_archivo)
         return resultado
 
+    # Operación Concreta
     def abrir_archivo(self, ruta_archivo):
         print(f"Abriendo el archivo '{ruta_archivo}'...")
 
+    # Operación Concreta
     def analizar_datos(self, datos):
         print("Analizando y estructurando la información obtenida...")
         return f"Reporte final basado en: {datos}"
 
+    # Operación Concreta
     def cerrar_archivo(self, ruta_archivo):
         print(f"Cerrando '{ruta_archivo}' y liberando recursos.")
         
+    # Operación Primitiva
+    @abstractmethod 
     def extraer_datos(self, ruta_archivo):
-        raise NotImplementedError("Método extraer_datos no implementado en la subclase")
+        pass
+    
+    # Operación Concreta
+    def requiere_analisis(self):
+        return True
 
+
+# ConcreteClass
 class ProcesadorCSV(ProcesadorDocumentos):
+    
+    # Implementación de la operación primitiva
     def extraer_datos(self, ruta_archivo):
         print("Extrayendo datos del archivo CSV...")
         return "[Datos del CSV]"
 
 
+# ConcreteClass
 class ProcesadorPDF(ProcesadorDocumentos):
+    
+    # Implementación de la operación primitiva  
     def extraer_datos(self, ruta_archivo):
-        print("Extrayendo texto e imágenes del PDF...")
+        print("Extrayendo texto del PDF...")
         return "[Texto del PDF]"
+    
+    # Sobrescritura de la Operación Concreta
+    def requiere_analisis(self):
+        return False
--- src/template/main.py
+++ src/template/main.py
@@ -1,15 +1,14 @@
-from __init__ import ProcesadorCSV, ProcesadorPDF
-
+# Client
 def main():
-    # Crear instancias de los procesadores específicos
     procesador_reportes = ProcesadorCSV()
     procesador_libros = ProcesadorPDF()
 
-    # Ejecutar el método plantilla
     resultado_1 = procesador_reportes.procesar_archivo("ventas_anuales.csv")
     print(f"Resultado 1: {resultado_1}\n")
 
     resultado_2 = procesador_libros.procesar_archivo("manual_usuario.pdf")
     print(f"Resultado 2: {resultado_2}\n")
+
+
 if __name__ == "__main__":
     main()
